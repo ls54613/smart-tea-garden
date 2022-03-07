281c62efe489cede3a4942e13cc7ae503fd5908e
WOTXH <709319433@qq.com>
2022-03-07 19:05:17 +0800

    no message

--- src/test/com/wanou/test/DemoTest.java
+++ src/test/com/wanou/test/DemoTest.java
@@ -20,9 +20,6 @@ import cn.hutool.http.HttpUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import com.alibaba.fastjson.JSONArray;
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGEncodeParam;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
 import com.wanou.common.utils.IdUtils;
 import com.wanou.project.system.domain.SysDept;
 import net.coobird.thumbnailator.Thumbnailator;
@@ -89,111 +86,19 @@ public class DemoTest {
 
     @Test
     public void test3() throws InterruptedException {
-        System.out.println("开始");
-        syncTest();
-        System.out.println("结束");
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(3);
+        list1.add(4);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(4);
+        list2.add(3);
+        System.out.println(CollUtil.subtract(list1,list2));
     }
 
     @Async
     public void syncTest() throws InterruptedException {
         Thread.sleep(1000L);
         System.out.println("异步执行");
-    }
-
-
-    public void updateImage(String inPath,String outPath,int wid) throws IOException {
-
-        InputStream inputStream = new FileInputStream(new File(inPath));
-
-        BufferedImage bufferedImage = ImageIO.read(inputStream);
-
-        int height = bufferedImage.getHeight(); //图片的高
-        int width = bufferedImage.getWidth();  //图片的宽
-
-        int newHeight =0;
-        int newWidth =0;
-        //
-        if(wid>width){
-            double s = wid/width;
-            newHeight = (int) ((int) height*s);
-            newWidth = (int) ((int) width*s);
-        }
-        if(wid<width){
-            double s = width/wid;
-            newHeight = (int) ((int) height/s);
-            newWidth = (int) ((int) width/s);
-        }
-
-
-        BufferedImage image = new BufferedImage(newWidth,newHeight, BufferedImage.TYPE_INT_BGR);
-
-        Graphics graphics = image.createGraphics();
-        graphics.drawImage(bufferedImage, 0, 0, newWidth, newHeight, null);
-
-        OutputStream outputStream = new FileOutputStream(new File(outPath));
-
-        JPEGImageEncoder j = JPEGCodec.createJPEGEncoder(outputStream);
-        j.encode(image);
-
-        outputStream.close();
-
-    }
-
-    /**
-     * 等比例压缩图片文件<br> 先保存原文件，再压缩、上传
-     * @param oldFile  要进行压缩的文件
-     * @param newFile  新文件
-     * @param width  宽度 //设置宽度时（高度传入0，等比例缩放）
-     * @param height 高度 //设置高度时（宽度传入0，等比例缩放）
-     * @param quality 质量
-     * @return 返回压缩后的文件的全路径
-     */
-    public static String zipImageFile(File oldFile,File newFile, int width, int height,
-                                      float quality) {
-        if (oldFile == null) {
-            return null;
-        }
-        try {
-            /** 对服务器上的临时文件进行处理 */
-            Image srcFile = ImageIO.read(oldFile);
-            int w = srcFile.getWidth(null);
-            //  System.out.println(w);
-            int h = srcFile.getHeight(null);
-            //  System.out.println(h);
-            double bili;
-            if(width>0){
-                bili=width/(double)w;
-                height = (int) (h*bili);
-            }else{
-                if(height>0){
-                    bili=height/(double)h;
-                    width = (int) (w*bili);
-                }
-            }
-            /** 宽,高设定 */
-            BufferedImage tag = new BufferedImage(width, height,
-                    BufferedImage.TYPE_INT_RGB);
-            tag.getGraphics().drawImage(srcFile, 0, 0, width, height, null);
-            //String filePrex = oldFile.getName().substring(0, oldFile.getName().indexOf('.'));
-            /** 压缩后的文件名 */
-            //newImage = filePrex + smallIcon+  oldFile.getName().substring(filePrex.length());
-
-            /** 压缩之后临时存放位置 */
-            FileOutputStream out = new FileOutputStream(newFile);
-
-            JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
-            JPEGEncodeParam jep = JPEGCodec.getDefaultJPEGEncodeParam(tag);
-            /** 压缩质量 */
-            jep.setQuality(quality, true);
-            encoder.encode(tag, jep);
-            out.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return newFile.getAbsolutePath();
     }
 
     @Test
