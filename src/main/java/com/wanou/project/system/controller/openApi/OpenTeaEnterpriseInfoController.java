024841e1984fc9ec8305f99d5074dbadc073b397
cx0809 <1660749047@qq.com>
2022-03-08 10:30:04 +0800

    no message

---
+++ src/main/java/com/wanou/project/system/controller/openApi/OpenTeaEnterpriseInfoController.java
@@ -0,0 +1,10 @@
+package com.wanou.project.system.controller.openApi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/openApi/teaEnterpriseInfo")
+public class OpenTeaEnterpriseInfoController {
+
+}
