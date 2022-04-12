acb10a1785aba750368cc67a17fbb07316537222
ls54613 <c>
2022-04-12 09:27:01 +0800

    Default Changelist

--- src/main/java/com/wanou/project/system/domain/VisualTianQi.java
+++ src/main/java/com/wanou/project/system/domain/VisualTianQi.java
@@ -25,4 +25,96 @@ public class VisualTianQi implements Serializable {
     private String nowPrecipitationStr; //当前降水量字符串
     private String windDirection; //风向
     private String pm25; //pm2.5
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getWeatherStr() {
+        return weatherStr;
+    }
+
+    public void setWeatherStr(String weatherStr) {
+        this.weatherStr = weatherStr;
+    }
+
+    public String getNowWeatherStr() {
+        return nowWeatherStr;
+    }
+
+    public void setNowWeatherStr(String nowWeatherStr) {
+        this.nowWeatherStr = nowWeatherStr;
+    }
+
+    public String getTempStr() {
+        return tempStr;
+    }
+
+    public void setTempStr(String tempStr) {
+        this.tempStr = tempStr;
+    }
+
+    public String getNowTemp() {
+        return nowTemp;
+    }
+
+    public void setNowTemp(String nowTemp) {
+        this.nowTemp = nowTemp;
+    }
+
+    public String getNowHumidity() {
+        return nowHumidity;
+    }
+
+    public void setNowHumidity(String nowHumidity) {
+        this.nowHumidity = nowHumidity;
+    }
+
+    public String getNowWindSpeedStr() {
+        return nowWindSpeedStr;
+    }
+
+    public void setNowWindSpeedStr(String nowWindSpeedStr) {
+        this.nowWindSpeedStr = nowWindSpeedStr;
+    }
+
+    public BigDecimal getNowWindSpeed() {
+        return nowWindSpeed;
+    }
+
+    public void setNowWindSpeed(BigDecimal nowWindSpeed) {
+        this.nowWindSpeed = nowWindSpeed;
+    }
+
+    public BigDecimal getNowPrecipitation() {
+        return nowPrecipitation;
+    }
+
+    public void setNowPrecipitation(BigDecimal nowPrecipitation) {
+        this.nowPrecipitation = nowPrecipitation;
+    }
+
+    public String getNowPrecipitationStr() {
+        return nowPrecipitationStr;
+    }
+
+    public void setNowPrecipitationStr(String nowPrecipitationStr) {
+        this.nowPrecipitationStr = nowPrecipitationStr;
+    }
+
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(String windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public String getPm25() {
+        return pm25;
+    }
+
+    public void setPm25(String pm25) {
+        this.pm25 = pm25;
+    }
 }
